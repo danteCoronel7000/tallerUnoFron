77ab99cce4fa036639cded66ee6cb8c47ff456af
danteCoronel7000 <coronel.iver.777@gmail.com>
2024-12-04 11:41:50 -0400

    tuneada g autores

--- src/app/business/gestionar-autores/components/layout-autores/layout-autores.component.ts
+++ src/app/business/gestionar-autores/components/layout-autores/layout-autores.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { HeadAutoresComponent } from "../head-autores/head-autores.component";
 import { ListAutoresComponent } from "../list-autores/list-autores.component";
 
 @Component({
   selector: 'app-layout-autores',
   standalone: true,
-  imports: [HeadAutoresComponent, ListAutoresComponent],
+  imports: [ListAutoresComponent],
   templateUrl: './layout-autores.component.html',
   styleUrl: './layout-autores.component.css'
 })
